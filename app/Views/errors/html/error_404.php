e63e5a376e6039809f9a1d35e9f10971136ceb87
Toni Haryanto <toha.samba@gmail.com>
2024-01-05 18:35:38 +0700

    Upgrade to CI 4.4.4

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,66 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-	<meta charset="utf-8">
-	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <title><?= lang('Errors.pageNotFound') ?></title>
 
-	<title>Pages / Not Found 404 - NiceAdmin Bootstrap Template</title>
-	<meta content="" name="description">
-	<meta content="" name="keywords">
-
-	<!-- Favicons -->
-	<link href="<?= base_url(); ?>/theme/assets/img/favicon.png" rel="icon">
-	<link href="<?= base_url(); ?>/theme/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-	<!-- Google Fonts -->
-	<link href="https://fonts.gstatic.com" rel="preconnect">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-	<!-- Vendor CSS Files -->
-	<link href="<?= base_url(); ?>/theme/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?= base_url(); ?>/theme/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-	<link href="<?= base_url(); ?>/theme/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-	<link href="<?= base_url(); ?>/theme/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-	<link href="<?= base_url(); ?>/theme/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-	<link href="<?= base_url(); ?>/theme/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-	<link href="<?= base_url(); ?>/theme/assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-	<!-- Template Main CSS File -->
-	<link href="<?= base_url(); ?>/theme/assets/css/style.css" rel="stylesheet">
-
-	<!-- =======================================================
-  * Template Name: NiceAdmin - v2.4.1
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+    <style>
+        div.logo {
+            height: 200px;
+            width: 155px;
+            display: inline-block;
+            opacity: 0.08;
+            position: absolute;
+            top: 2rem;
+            left: 50%;
+            margin-left: -73px;
+        }
+        body {
+            height: 100%;
+            background: #fafafa;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            color: #777;
+            font-weight: 300;
+        }
+        h1 {
+            font-weight: lighter;
+            letter-spacing: normal;
+            font-size: 3rem;
+            margin-top: 0;
+            margin-bottom: 0;
+            color: #222;
+        }
+        .wrap {
+            max-width: 1024px;
+            margin: 5rem auto;
+            padding: 2rem;
+            background: #fff;
+            text-align: center;
+            border: 1px solid #efefef;
+            border-radius: 0.5rem;
+            position: relative;
+        }
+        pre {
+            white-space: normal;
+            margin-top: 1.5rem;
+        }
+        code {
+            background: #fafafa;
+            border: 1px solid #efefef;
+            padding: 0.5rem 1rem;
+            border-radius: 5px;
+            display: block;
+        }
+        p {
+            margin-top: 1.5rem;
+        }
+        .footer {
+            margin-top: 2rem;
+            border-top: 1px solid #efefef;
+            padding: 1em 2em 0 2em;
+            font-size: 85%;
+            color: #999;
+        }
+        a:active,
+        a:link,
+        a:visited {
+            color: #dd4814;
+        }
+    </style>
 </head>
-
 <body>
+    <div class="wrap">
+        <h1>404</h1>
 
-	<main>
-		<div class="container">
-
-			<section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
-				<h1>404</h1>
-				<h2>The page you are looking for doesn't exist.</h2>
-				<a class="btn" href="<?= site_url(); ?>">Back to home</a>
-				<img src="<?= base_url(); ?>/theme/assets/img/not-found.svg" class="img-fluid py-5" alt="Page Not Found">
-				<div class="credits">
-					<!-- All the links in the footer should remain intact. -->
-					<!-- You can delete the links only if you purchased the pro version. -->
-					<!-- Licensing information: https://bootstrapmade.com/license/ -->
-					<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-					Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-				</div>
-			</section>
-
-		</div>
-	</main><!-- End #main -->
-
-	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
+        <p>
+            <?php if (ENVIRONMENT !== 'production') : ?>
+                <?= nl2br(esc($message)) ?>
+            <?php else : ?>
+                <?= lang('Errors.sorryCannotFind') ?>
+            <?php endif; ?>
+        </p>
+    </div>
 </body>
-
 </html>
