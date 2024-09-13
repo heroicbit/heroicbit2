a25d6fe2209d3a89cc24e89e45eaf79f5b36bf66
Toni Haryanto <toha.samba@gmail.com>
2024-09-13 13:54:59 +0700

    Change color theme

--- app/Views/template/mobilekit/partials/head.php
+++ app/Views/template/mobilekit/partials/head.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-<meta name="theme-color" content="#000000">
+<meta name="theme-color" content="#157CA1">
 <title></title>
 <meta name="description" content="Mobilekit HTML Mobile UI Kit">
 <meta name="keywords" content="bootstrap 5, mobile template, cordova, phonegap, mobile, html" />
