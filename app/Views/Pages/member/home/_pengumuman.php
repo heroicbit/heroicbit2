29a72ccbdac57588a3560a4b4a3b41a6c0000ed3
Toni Haryanto <toha.samba@gmail.com>
2024-10-29 18:15:03 +0700

    Centered

--- app/Views/Pages/member/home/_pengumuman.php
+++ app/Views/Pages/member/home/_pengumuman.php
@@ -1,9 +1,9 @@
 <div class="container pengumuman-home mb-5">
-    <div class="d-flex align-items-stretch">
+    <div class="d-flex align-items-stretch justify-content-center">
         <div class="rounded-4 bg-brand-2 d-flex align-items-center justify-content-center" style="width: 100px; border-right:1px dashed white;">
             <i class="fs-2 text-white-50 bi bi-megaphone"></i>
         </div>
-        <div class="card rounded-4 bg-brand-2">
+        <div class="rounded-4 bg-brand-2">
             <div class="p-2">
                 <a href="/member/pengumuman" class="item rounded-top-5 bg-brand-2 d-flex">
                     <div class="text-light">
