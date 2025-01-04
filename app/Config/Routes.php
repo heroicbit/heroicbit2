2bda101439d9ab472555cc6aef8c99c07cc00413
Toni Haryanto <toha.samba@gmail.com>
2025-01-04 14:53:00 +0700

    Create assetlinks.json for all sites

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 // $routes->get('/', 'Home::index');
-
-$routes->get('.well-known/assetlinks.json', 'Assetlink::index');
-
-// Override all non match uri to Heroic Page
-// $routes->match(['get', 'post'], '(:any)', '\Heroic\Controllers\Page::index');
