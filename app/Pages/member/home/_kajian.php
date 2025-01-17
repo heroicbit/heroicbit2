2e64698899a8d041e7e097affff541247251226a
Toni Haryanto <toha.samba@gmail.com>
2025-01-17 15:46:57 +0700

    CHange kajian thumbnail height

--- app/Pages/member/home/_kajian.php
+++ app/Pages/member/home/_kajian.php
@@ -34,7 +34,11 @@
                                     <div class="icon-video" x-show="article.youtube_url">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#EEEEEE" d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
                                     </div>
-                                    <img :src="article.medias[0].url" :alt="article.title" class="card-img-top swiper-thumbnail-image rounded-4"/>
+                                    <img 
+                                     :src="article.medias[0].url" 
+                                     :alt="article.title" 
+                                     class="card-img-top swiper-thumbnail-image rounded-4"
+                                     style="height:130px"/>
                                 </div>
                                 <div class="card-body text-dark py-3 px-2" style="min-height: 110px;">
                                     <div class="text-elipsis text-elipsis-3" style="line-height:1.1rem" x-text="article.title"></div>
